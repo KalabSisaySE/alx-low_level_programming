16dfeb01fddb4ec5f18ab6ede6fc714541834f72
KalabSisaySE <kyimusic11@gmail.com>
2022-03-24 05:39:05 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,17 @@
 #include "main.h"
-
 /**
- * _strcmp - Compares pointers to two strings.
- * @s1: A pointer to the first string to be compared.
- * @s2: A pointer to the second string to be compared.
+ * _strcmp - checks if 2 strings are of equal value
+ * @s1: destination string
+ * @s2: source string to be copied
  *
- * Return: If str1 < str2, the negative difference of the first unmatched characters.
- *         If str1 == str2, 0.
- *         If str1 > str2, the positive difference of the first unmatched characters.
+ * Return: difference of first characters that are of diff value
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
+	int j;
 
-	return (*s1 - *s2);
+	for (j = 0; s1[j] != '\0' && s2[j] != '\0'; j++)
+		if (s1[j] != s2[j])
+			return (s1[j] - s2[j]);
+	return (0);
 }
