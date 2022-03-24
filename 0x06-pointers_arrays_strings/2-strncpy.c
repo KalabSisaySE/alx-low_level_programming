fa3b2d4b180b583413f4bba2e24ac24ef676113c
KalabSisaySE <kyimusic11@gmail.com>
2022-03-24 00:00:56 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,8 +12,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
 
-	return(dest);
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i]	= src[i];
+	
+	while (i < n)
+		dest[i++] = '\0';
+	
+	return (dest);
 }
