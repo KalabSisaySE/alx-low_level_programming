a407661fc5e607899d7f40ef1a2180bb8a012678
KalabSisaySE <kyimusic11@gmail.com>
2022-03-23 23:49:01 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,5 +4,16 @@
  * *_strncpy - copies a string.
  * @dest: the string to be copied to.
  * @src: the string to be copied.
+ * @n: the maximmum number of bytes copied from src.
  *
- * Return: the 
+ * Return: pointer to the resulting string.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return(dest);
+}
