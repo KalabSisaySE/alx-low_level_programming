70ad96fb3496cca4b9be3cbb619fd0f756ece97b
KalabSisaySE <kyimusic11@gmail.com>
2022-03-24 07:40:38 -0700

    Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,42 +1,34 @@
-#include "bootcamp.h"
+#include "main.h"
 #include <stdio.h>
+
 /**
- * print_buffer - prints string and buffer sizes
- * @b: input pointer
- * @size: size of input buffer
- *
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
  * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
-	int i = 0, j = 0, count = 0;
+	int o, j, i;
 
-	while (j < size)
+	o = 0;
+
+	if (size <= 0)
 	{
-		printf("000000%02x: ", j);
-		count = 0;
-			while (count < 10)
-			{
-				if (i >= size)
-					printf("  ");
-				else
-					printf("%02x", *(b + i));
-				if (i % 2 != 0)
-					printf(" ");
-				i++;
-				count++;
-			}
-			i -= 10;
-			count = 0;
-			while (count < 10 && i < size)
-			{
-				printf("%c", *(b + i) > 31 && *(b + i) < 127 ? *(b + i) : '.');
-				count++;
-				i++;
-			}
-			if (j < size - 10)
-				printf("\n");
-		j += 10;
+		printf("\n");
+		return;
 	}
-	printf("\n");
-}
+	while (o < size)
+	{
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+
