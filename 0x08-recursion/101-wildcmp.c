20aa0435783f3ad0fa0f46fbeba2d4dbf67afa86
KalabSisaySE <kyimusic11@gmail.com>
2022-03-30 04:56:35 -0700

    Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,63 +1,85 @@
 #include "main.h"
-#define RIGHT 1
-#define LEFT -1
-#define TRUE (1 == 1)
-#define FALSE !TRUE
-#define MATCH 1
-#define NON_MATCH 0
-#define END_OF_STRING '\0'
-#define WILD_CARD '*'
+
+int strlen_no_wilds(char *str);
+void iterate_wild(char **wildstr);
+char *postfix_match(char *str, char *postfix);
+int wildcmp(char *s1, char *s2);
 
 /**
- * move_p - move pointer to 'character' in 'direction' seeking 'match'
- * @s1: check point to stop in rare circumstances
- * @s2: input string to move pointer from
- * @character: character to find next match or find first non-match of
- * @direction: direction to move pointer
- *     forward (d = RIGHT) or backwards (d = LEFT)
- * @match: find first match (m = MATCH) or non-match of c (m = NON_MATCH)
- * Return: pointer to match in input string
+ * strlen_no_wilds - Returns the length of a string,
+ *                   ignoring wildcard characters.
+ * @str: The string to be measured.
+ *
+ * Return: The length.
  */
-char *move_p(char *s1, char *s2, char character, int direction, int match)
+int strlen_no_wilds(char *str)
 {
-	if (s1 == s2 /* checks if string pointers are equal (used if s1 = s1 */
-	    || (match == MATCH && *s2 == character)
-	    || (match == NON_MATCH && *s2 != character))
-		return (s2);
-	return (move_p(s1,
-		       (direction == RIGHT ? s2 + 1 : s2 - 1),
-		       character,
-		       direction,
-		       match));
+	int len = 0, index = 0;
+
+	if (*(str + index))
+	{
+		if (*str != '*')
+			len++;
+
+		index++;
+		len += strlen_no_wilds(str + index);
+	}
+
+	return (len);
 }
+
 /**
- * wildcmp - checks is strings could be identical considering * wildcard
- * @s1: first string
- * @s2: second string
- * Return: boolean
+ * iterate_wild - Iterates through a string located at a wildcard
+ *                until it points to a non-wildcard character.
+ * @wildstr: The string to be iterated through.
+ */
+void iterate_wild(char **wildstr)
+{
+	if (**wildstr == '*')
+	{
+		(*wildstr)++;
+		iterate_wild(wildstr);
+	}
+}
+
+/**
+ * postfix_match - Checks if a string str matches the postfix of
+ *                 another string potentially containing wildcards.
+ * @str: The string to be matched.
+ * @postfix: The postfix.
+ *
+ * Return: If str and postfix are identical - a pointer to the null byte
+ *                                            located at the end of postfix.
+ *         Otherwise - a pointer to the first unmatched character in postfix.
+ */
+char *postfix_match(char *str, char *postfix)
+{
+	int str_len = strlen_no_wilds(str) - 1;
+	int postfix_len = strlen_no_wilds(postfix) - 1;
+
+	if (*postfix == '*')
+		iterate_wild(&postfix);
+
+	if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
+	{
+		postfix++;
+		return (postfix_match(str, postfix));
+	}
+
+	return (postfix);
+}
+
+/**
+ * wildcmp - Compares two strings, considering wildcard characters.
+ * @s1: The first string to be compared.
+ * @s2: The second string to be compared - may contain wildcards.
+ *
+ * Return: If the strings can be considered identical - 1.
+ *         Otherwise - 0.
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == END_OF_STRING && *s2 == END_OF_STRING)
-		return (TRUE);
-	if (*s2 == WILD_CARD)
+	if (*s2 == '*')
 	{
-		s2 = move_p(s1, s2, WILD_CARD, RIGHT, NON_MATCH);
-		if (*s2 == END_OF_STRING)
-			return (TRUE);
-		if (*s1 == END_OF_STRING)
-			return (FALSE);
-		/* recursively call wildcmp to see if next char is match */
-		if (wildcmp(move_p(s1, /* if no match, s1 is stop pointer*/
-				   /* below moves s1 pointer to end of string */
-				   move_p(s2, s1, END_OF_STRING, RIGHT, MATCH),
-				   *s2,
-				   LEFT,
-				   MATCH), /* move s1 back for a match of s2 */
-			    s2) == TRUE)
-			return (TRUE);
-	}
-	if (*s1 != *s2)
-		return (FALSE);
-	return (wildcmp(s1 + 1, s2 + 1));
-}
+		iterate_wild(&s2);
+
