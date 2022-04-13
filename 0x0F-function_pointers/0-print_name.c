98680949943ac60f91e07a97ed206bc54bcb8b8f
KalabSisaySE <kyimusic11@gmail.com>
2022-04-13 05:19:12 -0700

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 
-/*
+/**
  * print_name - prints the name it recieved based on the function argument.
  * @name: name to be printed.
  * @f: function used to print @name.
