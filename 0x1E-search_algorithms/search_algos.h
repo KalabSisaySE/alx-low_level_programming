68f1e6a4056abaa78d00837658f6af94ec4986bc
KalabSisaySE <kyimusic11@gmail.com>
2022-10-05 14:26:18 -0700

    update header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,5 @@
 #ifndef _SEARCH_ALGOS_H_
 #define _SEARCH_ALGOS_H_
-#include <stdlib.h>
 
 /**
  * struct skiplist_s - Singly linked list with an express lane
@@ -26,5 +25,7 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif /* SEARCH_ALGOS_H */ 
